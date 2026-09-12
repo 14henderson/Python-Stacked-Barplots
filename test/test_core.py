2d99e9223b589b82d03b7d5f3eed4c0ab697d0c9
Niklas Henderson <niklas.henderson@btinternet.com>
2026-09-12 17:39:26 +0100

    test(core): Simple unit test for core.py

--- test/test_core.py
+++ test/test_core.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from stackedbarplots.core import StackedBarplot
 from stackedbarplots.defaults import StackedPlotStyle
+from stackedbarplots.tools import ColourGradient
 
 class CoreClassTest(unittest.TestCase):
     """Unit tests for StackedBarplot class in core.py.
@@ -161,6 +162,8 @@ class CoreClassTest(unittest.TestCase):
                              (True, True, True, True)]:
             test_plot.style.fig["spinedisplay"] = spinedisplay
             test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_plot_legend(self):
         #fontsize; fontcolour; backgroundcolour; bordercolour;
@@ -186,6 +189,8 @@ class CoreClassTest(unittest.TestCase):
             for trans in [(0, 0), (.1, 0), (0, .1), (.1, .1)]:
                 test_plot.style.legend["transform"] = trans
                 test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_plot_vert_line(self):
         results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
@@ -207,29 +212,117 @@ class CoreClassTest(unittest.TestCase):
                 test_plot.style.vert_line["order"] = order
                 test_plot.render()
 
+        plt.close(test_plot.fig)
+        del test_plot
+
 class CoreStyleTest(unittest.TestCase):
     """Unit tests for StackedBarplot class in core.py.
     Unit tests in this class focus on the functional 
     plotting of different figure elements that the 
-    user is able to modify."""
+    user is able to modify. These are basic in nature
+    and only call each set_style method with example
+    data."""
 
     def test_style_bar_font(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_bar_labels_style(font_size=20, 
+                                       font_colour="red", 
+                                       font_colour_invert=True, 
+                                       bar_value_format="{0:.1f}%", 
+                                       display_thresh=(2, 10), 
+                                       padd_thresh=4, 
+                                       end_thresh_padd=True, 
+                                       align="right", 
+                                       padding=4)
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_bar(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        custom_colours = ColourGradient()
+        custom_colours.gradient(len(series_labels), (200, 100, 150), (100, 150, 200))
+        test_plot.set_bar_style(bar_height=.5, align="left", ordered="ascending", bar_gradient=custom_colours)
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_fig(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_fig_style(title="Test Title", 
+                                title_font_size=20, 
+                                title_colour="green", 
+                                font_family="Arial", 
+                                fig_size=(20, 20), 
+                                spine_display=(False, True, False, True))
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_axis(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_axis_style(x_lim=(-10, 50), 
+                                 step=5, 
+                                 x_font_size=10, 
+                                 y_font_size=10, 
+                                 x_axis_format="{0:.0}%", 
+                                 x_axis_show=True)
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_axis_title(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_axis_title_style(x_label="Example x axis label",
+                                       y_label="Example y axis label",
+                                       axis_label_font_colour="yellow")
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_legend(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_legend_style(show=True,
+                                   font_size=15,
+                                   spacing=.5,
+                                   font_colour="blue",
+                                   background_colour="yellow",
+                                   border_colour="black",
+                                   placement="below-horizontal",
+                                   marker_shape="o",
+                                   transform=(.1, -.1))
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
 
     def test_style_vertline(self):
-        pass
+        results = {"Category 1": [10, 5, 3, 11], "Category 2": [4, 2, 9, 12], "Category 3": [11, 12, 3, 4]}
+        series_labels = ["Series 1", "Series 2", "Series 3", "Series 4"]
+        test_plot = StackedBarplot(data=results, series_labels=series_labels)
+
+        test_plot.set_vert_line_style(show=True,
+                                      line_style="--",
+                                      colour="red",
+                                      alpha=.5,
+                                      order="front")
+        test_plot.render()
+        plt.close(test_plot.fig)
+        del test_plot
